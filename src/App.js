2bef588009075c50fdd5e8479217a3b1c55e88b0
Manuel <tofuah@gmail.com>
2021-10-04 11:24:36 +0100

    imported class component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassComponent from './component/ClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <ClassComponent />
+
+    </div>
+  );
+}
+
+export default App;
